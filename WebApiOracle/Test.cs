edf618a638ca05c9e36f56ab3203729270eaa2d3
umtbrdk <umtbrdk35@gmail.com>
2022-09-24 00:04:55 +0300

    test1

---
+++ WebApiOracle/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiOracle
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sa";
+        }
+
+        [HttpGet("{_a}")]
+        public string GetUmut(int _a)
+        {
+            return Hesapla(_a);
+        }
+
+        public string Hesapla(int sayi)
+        {
+            int a = sayi;
+            int sonuc = a + 2;
+            if (sonuc == 10)
+            {
+                return "abc";
+            }
+            else
+                return "bilmiyorum";
+        }
+    }
+}
